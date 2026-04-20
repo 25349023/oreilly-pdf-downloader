0b3b6735bb0a070c0dfc8402ff284426530bd417
25349023 <25349023.qq@gmail.com>
2026-04-21 00:29:57 +0800

    [UPDATE] setup loggers

--- main.py
+++ main.py
@@ -1,6 +1,50 @@
 import asyncio
+import logging.config
 
 from oreilly_pdf_downloader.book_downloader import BookDownloader
+
+
+def setup_logging():
+    LOGGING_CONFIG = {
+        'version': 1,
+        'disable_existing_loggers': False,  # avoid disabling loggers from other libraries
+        'formatters': {
+            'simple': {'format': '[%(levelname)s] %(message)s'},
+            'detailed': {
+                'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+                'datefmt': '%Y-%m-%d %H:%M:%S',
+            },
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'level': 'WARNING',
+                'formatter': 'simple',
+                'stream': 'ext://sys.stdout',
+            },
+            'file': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'level': 'DEBUG',
+                'formatter': 'detailed',
+                'filename': 'downloader.log',
+                'mode': 'a',
+                'encoding': 'utf-8',
+                'maxBytes': 5 * 1024 * 1024,  # 5 MB
+                'backupCount': 4,
+            },
+        },
+        'loggers': {
+            '': {  # Root Logger
+                'handlers': ['console', 'file'],
+                'level': 'DEBUG',
+            },
+        },
+    }
+
+    logging.config.dictConfig(LOGGING_CONFIG)
+    logger = logging.getLogger(__name__)
+
+    logger.info('Logging is set up.')
 
 
 async def main():
@@ -11,4 +55,5 @@ async def main():
 
 
 if __name__ == '__main__':
+    setup_logging()
     asyncio.run(main())
